c66d816a43cee0d4efeea3dfef38e6ce0492b56e
Youyu <youyuuzhang@gmail.com>
2022-04-18 11:26:50 -0700

    Youyu Revise Data Cleaning for getRelatedDataPlot
    Move the data cleaning part from inner function to outside.

--- www/function/function.R
+++ www/function/function.R
@@ -23,6 +23,16 @@ df_global <- read_excel(file.path(getwd(),"/www/data/lex-by-gapminder.xlsx"), sh
 gdp <- read_excel(file.path(getwd(),"/www/data/GM-GDP per capita - Dataset - v27.xlsx"), sheet = 'data-GDP-per-capita-in-columns', skip = 3) # GDP of each country since 1800
 earthquake <- read_csv(file.path(getwd(),"/www/data/earthquake_deaths_annual_number.csv")) # Earthquake death of each country since 1800
 
+# Data Cleaning
+# replace character into numerics, 10K -- 10000
+for (i in 2:length(colnames(earthquake))) {
+  if (sapply(earthquake,class)[i] == 'character'){
+    earthquake[[colnames(earthquake)[i]]] <- as.numeric(sub('k','e3',earthquake[[colnames(earthquake)[i]]]))
+  }
+  0}
+# delete rows with blank country name
+gdp_c <- gdp[!is.na(gdp[["Country Name"]]),]
+
 
 getWrodCloudPlot <- function(year){
   word<-na.omit(df1) 
@@ -96,22 +106,7 @@ getRelatedDataPlot <- function(related_country,start_date,end_date){
 }
 
 relation_gdp <- function(country) {
-  # Read raw data
-  #df <- read_excel(file.path(getwd(),"www/data/lex-by-gapminder.xlsx"), sheet = 2)  # Expected life of each country since 1800
-  #df_region <- read_excel(file.path(getwd(),"/www/data/lex-by-gapminder.xlsx"), sheet = 3)  # Expected life of each region since 1800
-  #df_global <- read_excel(file.path(getwd(),"/www/data/lex-by-gapminder.xlsx"), sheet = 4)  # Explected life global since 1800
-  #gdp <- read_excel(file.path(getwd(),"/www/data/GM-GDP per capita - Dataset - v27.xlsx"), sheet = 'data-GDP-per-capita-in-columns', skip = 3) # GDP of each country since 1800
-  #earthquake <- read_csv(file.path(getwd(),"/www/data/earthquake_deaths_annual_number.csv")) # Earthquake death of each country since 1800
   
-  # Data Cleaning
-  # replace character into numerics, 10K -- 10000
-  for (i in 2:length(colnames(earthquake))) {
-    if (sapply(earthquake,class)[i] == 'character'){
-      earthquake[[colnames(earthquake)[i]]] <- as.numeric(sub('k','e3',earthquake[[colnames(earthquake)[i]]]))
-    }
-    0}
-  # delete rows with blank country name
-  gdp_c <- gdp[!is.na(gdp[["Country Name"]]),]
   # connect df and gdp
   expectancy <- df[df[["geo.name"]] == country,]   # Pick the country
   geo <- expectancy$geo 
