4121d3f8d72d6a976c3901b6037d75c0dc677109
Youyu <youyuuzhang@gmail.com>
2022-04-18 11:17:05 -0700

    Youyu Adjusts Data Loading
    Revise global data import processing.

--- www/function/function.R
+++ www/function/function.R
@@ -15,9 +15,17 @@ library(wordcloud2)
 df1.path <- file.path("www/data/LifeExpectancy.csv")
 df.path <- file.path("www/data/LifeExpectancy.xlsx")
 
-df <- read.csv(df1.path)
+df1 <- read.csv(df1.path)
+# Read raw data
+df <- read_excel(file.path(getwd(),"www/data/lex-by-gapminder.xlsx"), sheet = 2)  # Expected life of each country since 1800
+df_region <- read_excel(file.path(getwd(),"/www/data/lex-by-gapminder.xlsx"), sheet = 3)  # Expected life of each region since 1800
+df_global <- read_excel(file.path(getwd(),"/www/data/lex-by-gapminder.xlsx"), sheet = 4)  # Explected life global since 1800
+gdp <- read_excel(file.path(getwd(),"/www/data/GM-GDP per capita - Dataset - v27.xlsx"), sheet = 'data-GDP-per-capita-in-columns', skip = 3) # GDP of each country since 1800
+earthquake <- read_csv(file.path(getwd(),"/www/data/earthquake_deaths_annual_number.csv")) # Earthquake death of each country since 1800
+
+
 getWrodCloudPlot <- function(year){
-  word<-na.omit(df) 
+  word<-na.omit(df1) 
   a<-data.frame(country = word$geo.name,
                 freq = (word[ , c(year)]))
   par(pin = c(5.5,5.5))
@@ -26,7 +34,7 @@ getWrodCloudPlot <- function(year){
 }
 
 getRankPlot <- function(top_year,top_rank){
-  word<-na.omit(df) 
+  word<-na.omit(df1) 
   top_year<-str_c("X",top_year)
   a<-data.frame(country = word$geo.name,
                 freq = word[ , c(top_year)])
@@ -89,11 +97,11 @@ getRelatedDataPlot <- function(related_country,start_date,end_date){
 
 relation_gdp <- function(country) {
   # Read raw data
-  df <- read_excel(file.path(getwd(),"www/data/lex-by-gapminder.xlsx"), sheet = 2)  # Expected life of each country since 1800
-  df_region <- read_excel(file.path(getwd(),"/www/data/lex-by-gapminder.xlsx"), sheet = 3)  # Expected life of each region since 1800
-  df_global <- read_excel(file.path(getwd(),"/www/data/lex-by-gapminder.xlsx"), sheet = 4)  # Explected life global since 1800
-  gdp <- read_excel(file.path(getwd(),"/www/data/GM-GDP per capita - Dataset - v27.xlsx"), sheet = 'data-GDP-per-capita-in-columns', skip = 3) # GDP of each country since 1800
-  earthquake <- read_csv(file.path(getwd(),"/www/data/earthquake_deaths_annual_number.csv")) # Earthquake death of each country since 1800
+  #df <- read_excel(file.path(getwd(),"www/data/lex-by-gapminder.xlsx"), sheet = 2)  # Expected life of each country since 1800
+  #df_region <- read_excel(file.path(getwd(),"/www/data/lex-by-gapminder.xlsx"), sheet = 3)  # Expected life of each region since 1800
+  #df_global <- read_excel(file.path(getwd(),"/www/data/lex-by-gapminder.xlsx"), sheet = 4)  # Explected life global since 1800
+  #gdp <- read_excel(file.path(getwd(),"/www/data/GM-GDP per capita - Dataset - v27.xlsx"), sheet = 'data-GDP-per-capita-in-columns', skip = 3) # GDP of each country since 1800
+  #earthquake <- read_csv(file.path(getwd(),"/www/data/earthquake_deaths_annual_number.csv")) # Earthquake death of each country since 1800
   
   # Data Cleaning
   # replace character into numerics, 10K -- 10000
