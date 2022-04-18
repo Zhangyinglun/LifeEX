7885c030da7ae6ecaf406cd1c3f7ed9358ff3c38
Youyu <55266460+kuohu233@users.noreply.github.com>
2022-04-17 19:13:06 -0700

    Youyu_Update function: getRelatedDataPlot
    Update function getRelatedDataPlot only. Didn't change other parts.

--- www/function/function.R
+++ www/function/function.R
@@ -30,6 +30,63 @@ getMapPlot <- function(date,position){
 #start_date：数据起始时间
 #end_date：数据结束时间
 getRelatedDataPlot <- function(related_country,start_date,end_date){
-  print(paste('Running getRelatedDataPlot! start_date=',start_date,'end_date=',end_date,'related_country=',related_country))
-  return(ggplot(iris)+geom_bar(aes(x = Species)))
+  print(paste(getwd()))
+  df <- read_excel(file.path(getwd(),"www/data/lex-by-gapminder.xlsx"), sheet = 2)
+  df_region <- read_excel(file.path(getwd(),"/www/data/lex-by-gapminder.xlsx"), sheet = 3)
+  df_global <- read_excel(file.path(getwd(),"/www/data/lex-by-gapminder.xlsx"), sheet = 4)
+  
+  
+  gdp <- read_excel(file.path(getwd(),"/www/data/GM-GDP per capita - Dataset - v27.xlsx"), sheet = 'data-GDP-per-capita-in-columns', skip = 3) 
+  
+  earthquake <- read_csv(file.path(getwd(),"/www/data/earthquake_deaths_annual_number.csv"))
+  
+  return(relation_gdp(df,related_country))
+  
+  #print(paste('Running getRelatedDataPlot! start_date=',start_date,'end_date=',end_date,'related_country=',related_country))
+  #return(ggplot(iris)+geom_bar(aes(x = Species)))
+}
+
+
+relation_gdp <- function(df,country) {
+  expectancy <- df[df[["geo.name"]] == country,]
+  geo <- expectancy$geo 
+  life <- expectancy[,5:length(expectancy)]
+  life[2,] <- NA
+  
+  if (any(grepl(geo, gdp_c$geo))) {
+    g <- gdp_c[gdp_c[['geo']] == geo,]
+    g <- g[,3:length(g)]
+    
+    for (i in 1:length(g)){
+      life[2,as.character(colnames(g)[i])] <- g[,i]
+    }
+    
+    life<- rbind(life, as.numeric(colnames(life)))
+    
+    life <- t(life)
+    colnames(life) <- c('expectlife','gdp','year')
+    
+    df_life <- data.frame(life)
+    
+    scaleFactor <- max(df_life$expectlife) / max(df_life$gdp, na.rm=TRUE)
+    
+    ggplot(data = df_life)+
+      geom_jitter(mapping = aes(x = year, y = expectlife, col="Expect Life"))+
+      geom_jitter(mapping = aes(x = year, y = gdp * scaleFactor, col = "GDP"))+
+      ggtitle((as.character(country)))+
+      
+      scale_y_continuous(
+        
+        # Features of the first axis
+        name = "Average Life Expectancy",
+        
+        # Add a second axis and specify its features
+        sec.axis=sec_axis(~./scaleFactor, name="GDP")
+      )
+    
+  } else{
+    print('No such country found in GDP record')
+  }
+  
+  
 }
