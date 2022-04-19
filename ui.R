e4488a5233bd43b1026755670427dda2ed7c02c1
Youyu <youyuuzhang@gmail.com>
2022-04-18 18:07:08 -0700

    Youyu Add a tab
    Add a tab to compare different countries at the same year.

--- ui.R
+++ ui.R
@@ -2,10 +2,6 @@ library(shinythemes)
 
 sidebar_panel_width = 3
 
-df <- read_excel(file.path(getwd(),"www/data/lex-by-gapminder.xlsx"), sheet = 2)  # Expected life of each country since 1800
-gdp <- read_excel(file.path(getwd(),"/www/data/GM-GDP per capita - Dataset - v27.xlsx"), sheet = 'data-GDP-per-capita-in-columns', skip = 3) # GDP of each country since 1800
-earthquake <- read_csv(file.path(getwd(),"/www/data/earthquake_deaths_annual_number.csv")) # Earthquake death of each country since 1800
-
 ui <- fluidPage(
   theme = shinytheme('spacelab'),
   navbarPage('Global Life Expectancy',
@@ -73,15 +69,19 @@ ui <- fluidPage(
                           conditionalPanel(
                             condition = "input.Relation == 'GDP Per Capita'",
                             selectInput(inputId = 'related_country2', label = 'Select Country', choice = gdp[['Country Name']] )),
-                          
-                          
-                            
-                          #selectInput(inputId = 'related_country', label = 'Select Country', choices = df[['geo.name']]),
-                          #textInput("related_country", label = 'Country', value = "China"),
-                          #dateRangeInput("related_dates", label = "Date range", start = '1990-01-01'),
+                        
                         ),
                         mainPanel(plotOutput('related_data_plot'))
                       )
-                    )
+                    ),
+             tabPanel("GDP vs Expected Life - Global",
+                      fluidPage(
+                        sidebarPanel(
+                          width = sidebar_panel_width,
+                          selectInput(inputId = 'time', label = 'Selecte related factor', choices = colnames(gdp_c)[3:length(colnames(gdp_c))]),
+                        ),
+                        mainPanel(plotOutput('year_plot'))
+                      )
+             )
   )
 )
